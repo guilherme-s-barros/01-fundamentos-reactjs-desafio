10b7dd03cabbf14aea18a8c3ec3fc05d85ee8e3c
Guilherme Barros <guilherme.s.barros@outlook.com>
2025-04-16 04:24:34 -0300

    feat: add animations to checkbox from task component and button from header component

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -1,11 +1,16 @@
+import { ChangeEvent } from 'react'
 import { Trash } from 'phosphor-react'
 
 import styles from './task.module.css'
 
 export function Task() {
+  function handleCheckTask(event: ChangeEvent<HTMLInputElement>) {
+    event.target.classList.add(styles.interacted)
+  }
+
   return (
     <li className={styles.task}>
-      <input type="checkbox" />
+      <input type="checkbox" onChange={handleCheckTask} />
       
       <p>
         Integer urna interdum massa libero auctor neque turpis
